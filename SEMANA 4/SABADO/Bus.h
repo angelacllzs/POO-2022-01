7c70cbc0b21b24e608b2813e561e2126a494b5c6
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-09-10 12:22:04 -0500

    Create Bus.h

---
+++ SEMANA 4/SABADO/Bus.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Movimiento.h"
+
+class CBus : public CMovimiento{
+
+public:
+	CBus() : CMovimiento(){
+		dx = rand() % 4 + 3;
+		ancho = 14; //EDITAMOS LUEGO DE DIBUJAR
+		alto = 3;
+	}
+	~CBus(){}
+
+	void borrar() {
+		Console::SetCursorPosition(x, y);     cout << "             ";
+		Console::SetCursorPosition(x, y + 1); cout << "             ";
+		Console::SetCursorPosition(x, y + 2); cout << "             ";
+	}
+
+	void dibujar() {
+		Console::SetCursorPosition(x, y);     cout << " _/_|[][][][]";
+		Console::SetCursorPosition(x, y + 1); cout << "(           |";
+		Console::SetCursorPosition(x, y + 2); cout << "=-OO     OO-=";
+	}
+
+};
