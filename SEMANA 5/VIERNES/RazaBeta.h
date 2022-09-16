b752e8021beab613cee1b3aa4d28c6f8bc8b983d
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-09-16 17:05:29 -0500

    Create RazaBeta.h

---
+++ SEMANA 5/VIERNES/RazaBeta.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Movimiento.h"
+
+class CRazaBeta : public CMovimiento{
+
+public:
+	CRazaBeta(int x, int y) : CMovimiento(x, y){
+		dy = rand() % 3 + 1;
+		ancho = 12;
+		alto = 3;
+	}
+	~CRazaBeta(){}
+
+	void borrar() {
+		Console::SetCursorPosition(x, y);     cout << "           ";
+		Console::SetCursorPosition(x, y + 1); cout << "           ";
+		Console::SetCursorPosition(x, y + 2); cout << "           ";
+	}
+
+	void dibujar() {
+		Console::SetCursorPosition(x, y);     cout << "   .---.   ";
+		Console::SetCursorPosition(x, y + 1); cout << " _/__~0_\_ ";
+		Console::SetCursorPosition(x, y + 2); cout << "(_________)";
+	}
+};
