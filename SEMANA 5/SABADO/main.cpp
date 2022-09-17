a53ab36eec6580c71354c5bcc4ed9094de335a44
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-09-17 12:57:10 -0500

    Create main.cpp

---
+++ SEMANA 5/SABADO/main.cpp
@@ -0,0 +1,35 @@
+#include "Controlador.h"
+
+int main() {
+	srand(time(NULL));
+	Console::CursorVisible = false;
+	char key;
+	CControlador* objControlador = new CControlador();
+
+	Console::SetCursorPosition(50, 1); cout << "COMIENZA INVASION...!!!";
+
+	while (objControlador->totalOvnis() != 10){
+		objControlador->borrarTodo();
+
+		if (_kbhit()) {
+			key = _getch();
+			key = toupper(key);
+			switch (key){
+			case 'A':
+				objControlador->agregarAlfa();
+				if (objControlador->getSizeAlfa() % 2 == 0) { objControlador->agregarBeta(); }
+				if (objControlador->getSizeBeta() % 2 == 0 && objControlador->getSizeAlfa() % 4 == 0) { objControlador->agregarGamma(); }
+				break;
+			}
+		}
+		objControlador->moverTodo();
+		objControlador->dibujarTodo();
+
+		_sleep(100);
+	}
+
+	cout << "HEMOS SIDO INVADIDOS...!!! :(";
+
+	_getch();
+	return 0;
+}
