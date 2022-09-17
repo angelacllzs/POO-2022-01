e9542dcb8ccf6689465483d86db87cc505ed2762
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-09-17 12:57:20 -0500

    Create RazaGamma.h

---
+++ SEMANA 5/SABADO/RazaGamma.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Movimiento.h"
+
+class CRazaGamma : public CMovimiento{
+private:
+
+public:
+	CRazaGamma(int x,int y) : CMovimiento(x,y){
+		dy = rand() % 4 + 1;
+		ancho = 20; //EDITAR LUEGO DE DIBUJAR
+		alto = 9; //EDITAR LUEGO DE DIBUJAR
+	}
+	~CRazaGamma(){}
+
+	void borrar() {
+		Console::SetCursorPosition(x, y);     cout << "                   ";
+		Console::SetCursorPosition(x, y + 1); cout << "                   ";
+		Console::SetCursorPosition(x, y + 2); cout << "                   ";
+		Console::SetCursorPosition(x, y + 3); cout << "                   ";
+		Console::SetCursorPosition(x, y + 4); cout << "                   ";
+		Console::SetCursorPosition(x, y + 5); cout << "                   ";
+		Console::SetCursorPosition(x, y + 6); cout << "                   ";
+		Console::SetCursorPosition(x, y + 7); cout << "                   ";
+		Console::SetCursorPosition(x, y + 8); cout << "                   ";
+	}
+
+	void dibujar() {
+		Console::SetCursorPosition(x, y);     cout << "         .         ";
+		Console::SetCursorPosition(x, y + 1); cout << "         |         ";
+		Console::SetCursorPosition(x, y + 2); cout << "     .-''^''-.     ";
+		Console::SetCursorPosition(x, y + 3); cout << "  ,,./_....._\.,,  ";
+		Console::SetCursorPosition(x, y + 4); cout << " .-             -. ";
+		Console::SetCursorPosition(x, y + 5); cout << "(  000  000  000  )";
+		Console::SetCursorPosition(x, y + 6); cout << " '-.,__________,.-'";
+		Console::SetCursorPosition(x, y + 7); cout << "     /        \    ";
+		Console::SetCursorPosition(x, y + 8); cout << "   _/          \_  ";
+	}
+};
