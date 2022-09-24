74d17d237a2278750c521f5646b42ea381955c88
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-09-24 13:10:11 -0500

    Create Bicicleta.h

---
+++ SEMANA 6/SABADO/Bicicleta.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Movimiento.h"
+
+class CBicicleta : public CMovimiento{
+public:
+	CBicicleta() : CMovimiento(){
+		dx = 1;
+		ancho = 7;
+		alto = 2;
+	}
+	~CBicicleta(){}
+
+	void borrar() {
+		Console::SetCursorPosition(x, y);     cout << "      ";
+		Console::SetCursorPosition(x, y + 1); cout << "      ";
+	}
+
+	void dibujar() {
+		Console::SetCursorPosition(x, y);     cout << " .._\ ";
+		Console::SetCursorPosition(x, y + 1); cout << "(o)(o)";
+	}
+};
