def0a1742faddd4dd338aee7aeb5e07d9e619225
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-10-01 12:55:42 -0500

    Create Persona.h

---
+++ SEMANA 7/SABADO/Persona.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Caracter.h"
+
+class CPersona : public CCaracter{
+private:
+	int vida;
+	bool inmunidad;
+	int cantMov;
+public:
+	CPersona(int x, int y) : CCaracter(x, y){
+		dx = 1;
+		dy = 1;
+		ancho = 2;
+		alto = 2;
+		inmunidad = false;
+		cantMov = 0;
+		vida = 3;
+	}
+	~CPersona(){}
+
+	int getVida() { return vida; }
+	bool getInmunidad() { return inmunidad; }
+	int getCantMov() { return cantMov; }
+
+	void setInmunidad(bool i) { inmunidad = i; }
+	void setCantMov(int c) { cantMov = c; }
+	void disminuirVida() { vida--; }
+
+	void mover(char key) {
+		switch (key){
+		case 'A':
+			if (x + dx >= 0) x -= dx;
+			cantMov--;
+			break;
+		case 'D':
+			if (x + dx + ancho <= 70) x += dx;
+			cantMov--;
+			break;
+		case 'W':
+			if (y + dy >= 0) y -= dy;
+			cantMov--;
+			break;
+		case 'S':
+			if (y + dy + alto <= 50) y += dy;
+			cantMov--;
+			break;
+		}
+	}
+
+	void borrar() {
+		Console::SetCursorPosition(x, y);     cout << " ";
+		Console::SetCursorPosition(x, y + 1); cout << " ";
+	}
+
+	void dibujar() {
+		Console::SetCursorPosition(x, y);     cout << "@";
+		Console::SetCursorPosition(x, y + 1); cout << char(197);
+	}
+};
