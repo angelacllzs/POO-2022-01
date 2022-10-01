e3ec1eeabedf548c4a02675082884c621dd3892c
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2022-10-01 12:55:35 -0500

    Create Controlador.h

---
+++ SEMANA 7/SABADO/Controlador.h
@@ -0,0 +1,117 @@
+#pragma once
+#include "Persona.h"
+#include "Vacuna.h"
+#include "Virus.h"
+#include "Casa.h"
+#include <vector>
+
+class CControlador{
+private:
+	CPersona* objPersona;
+	vector<CVacuna*> vecVacuna;
+	vector<CVirus*> vecVirus;
+	CCasa* objCasa;
+
+public:
+	CControlador(int cantVirus) {
+		objPersona = new CPersona(4, 4);
+		objCasa = new CCasa(60, 40);
+		for (int i = 0; i < cantVirus; i++){
+			vecVirus.push_back(new CVirus());
+		}
+	}
+	~CControlador();
+
+	void agregarVacuna() {
+		CVacuna* objVacuna = new CVacuna();
+		if (vecVacuna.size() < 5) {
+			vecVacuna.push_back(objVacuna);
+		}
+	}
+
+	void borrarTodo() {
+		objPersona->borrar();
+		objCasa->borrar();
+		for (int i = 0; i < vecVirus.size(); i++){
+			vecVirus[i]->borrar();
+		}
+		for (int i = 0; i < vecVacuna.size(); i++) {
+			vecVacuna[i]->borrar();
+		}
+	}
+
+	void moverTodo() {
+		for (int i = 0; i < vecVirus.size(); i++) {
+			vecVirus[i]->mover();
+		}
+		for (int i = 0; i < vecVacuna.size(); i++) {
+			vecVacuna[i]->mover();
+		}
+	}
+
+	void dibujarTodo() {
+		objPersona->dibujar();
+		objCasa->dibujar();
+		for (int i = 0; i < vecVirus.size(); i++) {
+			vecVirus[i]->dibujar();
+		}
+		for (int i = 0; i < vecVacuna.size(); i++) {
+			vecVacuna[i]->dibujar();
+		}
+	}
+
+//--------------COLISIONES----------------------
+
+	bool personaColisionVirus() {
+		if (objPersona->getInmunidad() != true) { // == false
+			for (int i = 0; i < vecVirus.size(); i++){
+				//OBTIENDO Y ASIGNANDO VALORES PARA PERSONA
+				int x1 = objPersona->getX(); int y1 = objPersona->getY();
+				int ancho1 = objPersona->getAncho(); int alto1 = objPersona->getAlto();
+				//OBTIENDO Y ASIGNANDO VALORES PARA VIRUS
+				int x2 = vecVirus[i]->getX(); int y2 = vecVirus[i]->getY();
+				int ancho2 = vecVirus[i]->getAncho(); int alto2 = vecVirus[i]->getAlto();
+				//VALIDANDO COLISION
+				if (!(x1 + ancho1 < x2 || y1 + alto1 < y2 || x1 > x2 + ancho2 || y1 > y2 + alto2)) {
+					objPersona->disminuirVida();
+					objPersona->setX(4);
+					objPersona->setY(4);
+					if (objPersona->getVida() == 0) {
+						return true;
+					}
+				}
+			}
+		}
+	}
+
+	void personaColisionVacuna() {
+		for (int i = 0; i < vecVacuna.size(); i++) {
+			//OBTIENDO Y ASIGNANDO VALORES PARA PERSONA
+			int x1 = objPersona->getX(); int y1 = objPersona->getY();
+			int ancho1 = objPersona->getAncho(); int alto1 = objPersona->getAlto();
+			//OBTIENDO Y ASIGNANDO VALORES PARA VACUNA
+			int x2 = vecVacuna[i]->getX(); int y2 = vecVacuna[i]->getY();
+			int ancho2 = vecVacuna[i]->getAncho(); int alto2 = vecVacuna[i]->getAlto();
+			//VALIDANDO COLISION
+			if (!(x1 + ancho1 < x2 || y1 + alto1 < y2 || x1 > x2 + ancho2 || y1 > y2 + alto2)) {
+				objPersona->setInmunidad(true);
+				objPersona->setCantMov(10);
+			}
+		}
+	}
+
+	bool personaColisionCasa() {
+		//OBTIENDO Y ASIGNANDO VALORES PARA PERSONA
+		int x1 = objPersona->getX(); int y1 = objPersona->getY();
+		int ancho1 = objPersona->getAncho(); int alto1 = objPersona->getAlto();
+		//OBTIENDO Y ASIGNANDO VALORES PARA CASA
+		int x2 = objCasa->getX(); int y2 = objCasa->getY();
+		int ancho2 = objCasa->getAncho(); int alto2 = objCasa->getAlto();
+		//VALIDANDO COLISION
+		if (!(x1 + ancho1 < x2 || y1 + alto1 < y2 || x1 > x2 + ancho2 || y1 > y2 + alto2)) {
+			return true;
+		}
+	}
+
+	CPersona* getPersona() { return objPersona; }
+};
